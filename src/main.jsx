d6895903569b9a55561649ab9cf52bac5c96d41f
Luis David <cusiluis04@gmail.com>
2023-12-22 00:27:28 -0500

    Aprendiendo a renderizar en React

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+<React.Fragment>
+  <button>Agregar</button>
+  <button>Agregar</button>
+  <button>Agregar</button>
+</React.Fragment>
+)
