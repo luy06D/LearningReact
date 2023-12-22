9f783f122fa749ee653cc7053dde3d834a6affcc
Luis David <cusiluis04@gmail.com>
2023-12-22 11:43:16 -0500

    Reutilizando componentes

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,24 @@ import ReactDOM from 'react-dom/client'
 
  const root = ReactDOM.createRoot(document.getElementById('root'))
 
+ // Creamos una funcion que retorne botones y reutilizar
+ const createButton = ({text}) =>{
+  return (
+    <button>
+      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 20">
+    <path d="M18.012 13.453c-.219-1.173-2.163-1.416-2.6-3.76l-.041-.217c0 .006 0-.005-.007-.038v.021l-.017-.09-.005-.025v-.006l-.265-1.418a5.406 5.406 0 0 0-5.051-4.408.973.973 0 0 0 0-.108L9.6 1.082a1 1 0 0 0-1.967.367l.434 2.325a.863.863 0 0 0 .039.1A5.409 5.409 0 0 0 4.992 9.81l.266 1.418c0-.012 0 0 .007.037v-.007l.006.032.009.046v-.01l.007.038.04.215c.439 2.345-1.286 3.275-1.067 4.447.11.586.22 1.173.749 1.074l12.7-2.377c.523-.098.413-.684.303-1.27ZM1.917 9.191h-.074a1 1 0 0 1-.924-1.07 9.446 9.446 0 0 1 2.426-5.648 1 1 0 1 1 1.482 1.343 7.466 7.466 0 0 0-1.914 4.449 1 1 0 0 1-.996.926Zm5.339 8.545A3.438 3.438 0 0 0 10 19.1a3.478 3.478 0 0 0 3.334-2.5l-6.078 1.136Z"/>
+  </svg>
+  {text}</button>
+
+  )
+ }
+
+ // Renderizar elementos
 root.render(
 <React.Fragment>
-  <button>Agregar</button>
-  <button>Agregar</button>
-  <button>Agregar</button>
+  {createButton({text: 'Boton agregar'})}
+  {createButton({text: 'Boton eliminar'})}
+  {createButton({text: 'Boton actualizar'})}
+
 </React.Fragment>
 )
