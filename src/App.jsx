f115780b5118eea216bdfe18508639ba351031db
Luis David <cusiluis04@gmail.com>
2023-12-27 00:48:39 -0500

    Creando primer componente App

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+// Exportamos el componente App para el main.jsx
+export function App(){
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/midudev" alt="El avatar" />
+                <div>
+                    <strong>Luis David Cusi Gonzales</strong>
+                    <span>@cusiluis06</span>
+                </div>
+            </header>      
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>      
+        </article>
+        
+    )
+}
+
